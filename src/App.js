2732cc32733b4b26e47df4eb7a6b185a821a9d55
Mikko Nuuttila <mikkonuuttila@Mikkos-MacBook-Pro.local>
2024-10-13 07:27:12 +0300

    App.js päivitetty teksti muutoksia
    git commit --amend

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hungry Hippos</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Gotta eat something
         </p>
         <a
           className="App-link"
